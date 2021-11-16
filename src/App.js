e2fb0f56bede8d8841f4869dbbd0d193624296ed
Kaique Borges <rique.kaique@gmail.com>
2021-11-16 18:24:37 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const API_URL = "https://api.quotable.io/random";
+
+function Quote(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p id="text">
+        <span>"</span>
+        {props.text}
+        <span>"</span>
+      </p>
+      <span id="author">{props.author}</span>
+    </>
   );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      author: "",
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    const { text, author } = await this.getQuote();
+    this.setState({
+      text,
+      author,
+    });
+  }
+
+  async getQuote() {
+    try {
+      let data;
+      await fetch(API_URL)
+        .then((response) => response.json())
+        .then((body) => (data = { author: body.author, text: body.content }));
+      return data;
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  handleClick() {
+    this.getQuote().then(({ text, author }) => {
+      this.setState({
+        text,
+        author,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <Quote text={this.state.text} author={this.state.author} />
+        <div id="controls">
+          <a
+            href={`https://twitter.com/intent/tweet?text=${this.state.text}`}
+            id="tweet-quote"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tweet this quote
+          </a>
+          <button id="new-quote" onClick={this.handleClick}>
+            New Quote
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
